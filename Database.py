aeda3e8c53f9dd68d81925e0b2d2ed02076449a8
Sahil Katve <sahilkatve7@gmail.com>
2026-01-29 15:54:20 +0530

    Add files via upload

---
+++ Database.py
@@ -0,0 +1,19 @@
+import psycopg2
+import os
+
+def get_connection():
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    if not DATABASE_URL:
+        raise ValueError("DATABASE_URL environment variable not set")
+    return psycopg2.connect(
+        DATABASE_URL,
+        sslmode='require'
+)
+
+if __name__ == "__main__":
+    print("Database connected successfully")
+    
+print("HOST:", os.getenv("DB_HOST"))
+print("DB NAME:", os.getenv("DB_NAME"))
+print("USER:", os.getenv("DB_USER"))
+print("PORT:", os.getenv("DB_PORT"))
